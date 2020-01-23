475ed5609c2ac17bccfe997b56627e7b52f5d6be
marwan jassim <marwanjassim87@gmail.com>
2020-01-22 19:50:33 -0800

    set up server to use controllers

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
+var router = require("./controller/burgers_controller.js");
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.use("/", router);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
